7206ae454f778b6386472ee044a3d3c1425601b0
zhengzhigang <zhengzhigang@topnet.net.cn>
2026-02-03 14:12:57 +0800

    feat: 修改默认诗人

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -228,7 +228,7 @@ onMounted(async () => {
     //如果没有传authorId，如果没有传personId就默认是李白，如果传了personid就用传的personid,authorid就没有了
     if (!route.query.personId) {
       datas.personId = JSONBig.parse("32540");
-      datas.authorId = JSONBig.parse("1656658312757964828");
+      datas.authorId = JSONBig.parse("2016760907028709538");
     } else {
       datas.personId = JSONBig.parse(route.query.personId);
       await findAuthorIdByPersonId({ personId: datas.personId }).then((res) => {
